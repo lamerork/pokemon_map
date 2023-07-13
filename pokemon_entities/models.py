366a918a964beaebb11eb27cba9282ea72c9dc2c
Anton Ozerov <lamerork@gmail.com>
2023-07-13 17:56:45 +0300

    add verbose_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,14 +1,12 @@
-from django.db import models  # noqa F401
-
-# your models here
+from django.db import models  
 
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name='Имя покемона')
     title_en = models.CharField(max_length=200, blank=True, null=True, verbose_name='Имя покемона на EN')
     title_jp = models.CharField(max_length=200, blank=True, null=True, verbose_name='Имя покемона на JP')
-    image = models.ImageField(blank=True, null=True, verbose_name='Картинка', upload_to='images')
-    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, verbose_name='Картинка покемона', upload_to='images')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание покемона')
     previous_evolution = models.ForeignKey('self',
                                            verbose_name='Из кого эволюционировал',
                                            null=True,
@@ -23,7 +21,7 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, related_name="entities", on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, related_name="entities", on_delete=models.CASCADE, verbose_name='Покемон')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Время появления')
